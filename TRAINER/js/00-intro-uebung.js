f2c88489b0412950716670d32ec86b38db8e7254
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-07-27 11:20:40 +0200

    m00 übungslösung

--- TRAINER/js/00-intro-uebung.js
+++ TRAINER/js/00-intro-uebung.js
@@ -5,4 +5,12 @@
 Erstellen Sie eine Mini-Anwendung, die nach einem Benutzernamen und nach einem Passwort fragt und dann den Benutzer begrüßt.
 Jedes Passwort wird erstmal akzeptiert.
 Viel Erfolg!
- */ 
+*/
+function login() {
+    // let u: string = (document.getElementById('user')!).value; // Property 'value' does not exist on type 'HTMLElement'.
+    // let u: string = (<HTMLInputElement> document.getElementById('user')).textContent!; // mit Assertion zu HTMLInputElement ist die Option mit null weg
+    // aber textContent ist 'string | null'
+    // und textContent ist bei HTMLInputElement leer.
+    var u = document.getElementById('user').value;
+    document.getElementById('hi').textContent = "Hello " + u;
+}
