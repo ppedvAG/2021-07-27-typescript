cf2eaae87a9364b538454a9bb4a3d11836dcadbe
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-07-27 16:59:11 +0200

    m003 demo

---
+++ TRAINER/js/03-functions.js
@@ -0,0 +1,32 @@
+"use strict";
+// TIMING FUNCTIONS
+// setInterval(() => {}, 1000);
+// setTimeout(() => {}, 1000);
+// OPTIONALE ARGUMENTE
+// function buildName (firstName: string, middleName?: string, lastName: string): string { // A required parameter cannot follow an optional parameter.ts(1016)
+function buildName(firstName, lastName, middleName) {
+    return `${firstName} ${middleName} ${lastName}`;
+}
+console.log("buildName('Max', 'Mustermann') :>> ", buildName('Max', 'Mustermann')); // Max undefined Mustermann
+// DEFAULT VALUES
+function buildName2(firstName, lastName, middleName = "") {
+    return `${firstName} ${middleName} ${lastName}`;
+}
+console.log("buildName2('Max', 'Mustermann') :>> ", buildName2('Max', 'Mustermann')); //  Max  Mustermann
+console.log("buildName2('Max', 'Mustermann', 'Peter') :>> ", buildName2('Max', 'Mustermann', 'Peter')); //  Max Peter Mustermann
+// UNDIFINED NUMBER OF ARGS
+function giveNodeNames(...args) {
+    console.log('args :>> ', args);
+    let arrayOfNodeNames = [];
+    if (args[0].nodeName) {
+        arrayOfNodeNames.push(args[0].nodeName);
+    }
+    else {
+        for (const iterator of args[0]) {
+            arrayOfNodeNames.push(iterator);
+        }
+    }
+    return arrayOfNodeNames;
+}
+let nodeNamesOfClassTest = giveNodeNames(document.getElementsByClassName("test"));
+console.log('nodeNamesOfClassTest :>> ', nodeNamesOfClassTest);
